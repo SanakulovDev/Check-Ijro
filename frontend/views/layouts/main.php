284a1bd8d2fa6b810c838c9aa418cc9ebbbca94b
SanakulovDev <sanakulovdev@gmail.com>
2024-11-18 20:56:12 +0500

    "Updated body class and reCAPTCHA overlay ID in main.php layout"

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -106,10 +106,10 @@ AppAsset::register($this);
 <?php
 $isHuman = Yii::$app->session->get('isHuman') || Yii::$app->request->cookies->getValue('isHuman');
 ?>
-<body class="d-flex flex-column h-100 <?= $isHuman ? '' : 'recaptcha-active1' ?> bg-light">
+<body class="d-flex flex-column h-100 <?= $isHuman ? '' : 'recaptcha-active' ?> bg-light">
     <?php $this->beginBody() ?>
     <!-- reCAPTCHA Overlay -->
-    <div id="recaptcha-overlay1" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: #fff; z-index: 9999; display: flex; align-items: center; justify-content: center;">
+    <div id="recaptcha-overlay" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: #fff; z-index: 9999; display: flex; align-items: center; justify-content: center;">
         <div id="recaptcha-container">
             <div id="recaptcha"></div>
         </div>
