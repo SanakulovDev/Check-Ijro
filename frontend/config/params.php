a76378163c29fb3f0502fe9922661452e4e4406e
SanakulovDev <sanakulovdev@gmail.com>
2024-11-18 16:54:18 +0500

    Added recaptcha site key and secret key to params.php

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,5 +1,7 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'domainName'    =>  ''
+    'domainName'    =>  '',
+    'recaptChaSiteKey' => '6LfNm4IqAAAAALl9PcuV4gazAYvicz4xE3vRrzHi',
+    'recatchaSecretKey' => '6LfNm4IqAAAAALVeAQAHqNet7t48YDnAtEvHzq-C'
 ];
