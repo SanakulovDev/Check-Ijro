851b2c2592a96c577907ceb883d0549fa96c7139
SanakulovDev <sanakulovdev@gmail.com>
2024-11-21 15:41:25 +0500

    Added favicon link to main layout.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png">
     <style>
         #loader {
             display: flex;
